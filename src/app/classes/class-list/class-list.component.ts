88e7b60c55811983a39f0def9974ad41d5644805
FabianHaeubl <fabian.haeubl@gmail.com>
2021-11-11 09:33:05 +0100

    3 files modified

--- src/app/classes/class-list/class-list.component.ts
+++ src/app/classes/class-list/class-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from '../../services/data-service.service';
 
 @Component({
@@ -9,9 +10,13 @@ import { DataService } from '../../services/data-service.service';
 export class ClassListComponent implements OnInit {
   classNames: Array<string>;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit(): void {
     this.classNames = this.dataService.getClassNames();
+  }
+
+  openDetail(cn: string): void {
+    this.router.navigateByUrl('/class-detail?classname=' + cn);
   }
 }
